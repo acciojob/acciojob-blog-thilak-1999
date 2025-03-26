721140d4ae57d6fd3620b8a566ec90c708eeb37e
Thilak Saravanan <191076183+thilak-1999@users.noreply.github.com>
2025-03-26 08:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your JS code here. If required.
-https://www.svgrepo.com/show/345221/three-dots.svg
+https://www.svgrepo.com/show/345221/three-dots.svgsplitter
+CSS
+
+1
+2
+/*  your css code here. If applicable */
+
+
+Javascript
+
+1
+2
